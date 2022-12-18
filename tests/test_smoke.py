9223f9f41f3ef603d794ad51735757bb43f35b87
dmitryhusev <dimtri.gusev@gmail.com>
2022-12-18 17:37:27 +0100

    28

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,11 +1,14 @@
-
+from screen import make_attachment
+from time import sleep
 
 
 def test_smoke_1(browser):
     browser.get('https://pytest-html.readthedocs.io/en/latest/user_guide.html')
-    assert 1
+    sleep(5)
+    make_attachment(browser)
 
 
 def test_smoke_2(browser):
     browser.get('https://booqua.de/basket.html')
-    assert 1
+    sleep(5)
+    make_attachment(browser)
