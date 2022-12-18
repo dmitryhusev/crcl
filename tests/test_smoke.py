3dc6baa25624dcbf5a202ba5b3b08acbcb56ee63
dmitryhusev <dimtri.gusev@gmail.com>
2022-12-18 14:49:03 +0100

    2

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,13 +1,10 @@
-import pytest
 
 
-@pytest.mark.smoke
 def test_smoke_1(browser):
     browser.get('https://pytest-html.readthedocs.io/en/latest/user_guide.html')
     assert 1
 
 
-@pytest.mark.smoke
 def test_smoke_2(browser):
     browser.get('https://booqua.de/basket.html')
     assert 1
